7089d3121245a1348219aeb38a713c37e63cd973
Robbie Jaeger <jaeger.rob@gmail.com>
2019-02-28 14:50:18 -0700

    Add html and js boilerplate using server static files

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(express.static('public'));
 
 app.listen(app.get('port'), () => {
-  console.log(`Notifications app running on http:localhost:${app.get('port')}`);
+  console.log(`Notifications app running on http://localhost:${app.get('port')}`);
 });
