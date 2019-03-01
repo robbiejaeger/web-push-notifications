6b105c02ff44548e4c48907a9bdae1c5f0bf8182
Robbie Jaeger <jaeger.rob@gmail.com>
2019-03-01 16:28:53 -0700

    Add labels for sw and push notification errors

--- public/index.js
+++ public/index.js
@@ -7,7 +7,7 @@ const showNotification = (title, options) => {
       .then(registration => {
         registration.showNotification(title, options);
       })
-      .catch(error => console.error(error));
+      .catch(error => console.error('Notification error:', error));
   }
 }
 
@@ -31,6 +31,6 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('./service-worker.js')
       .then(registration => console.log('Service worker registered.'))
-      .catch(error => console.error(error));
+      .catch(error => console.error('SW Registration error', error));
   });
 }
