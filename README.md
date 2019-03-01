66cf9d7ff80706852cb63bc35e6249669b0865c8
Robbie Jaeger <jaeger.rob@gmail.com>
2019-03-01 16:24:34 -0700

    Add push notification permission and notification for button click

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Experimenting with the Web Push and Push Notifications API.
 
 Using these [Google Dev docs](https://developers.google.com/web/ilt/pwa/introduction-to-push-notifications#push_api) as a resource.
+
+There is no caching strategy for this application - the focus was for Web Push and Push Notifications.
--- public/index.js
+++ public/index.js
@@ -1,7 +1,36 @@
+const permissionBtn = document.getElementById('permission');
+const notifyBtn = document.getElementById('notify');
+
+const showNotification = (title, options) => {
+  if (Notification.permission === 'granted') {
+    navigator.serviceWorker.getRegistration()
+      .then(registration => {
+        registration.showNotification(title, options);
+      })
+      .catch(error => console.error(error));
+  }
+}
+
+const requestPermission = () => {
+  Notification.requestPermission(status => {
+    console.log('Push notification permission:', status);
+  });
+}
+
+permissionBtn.addEventListener('click', requestPermission);
+
+notifyBtn.addEventListener('click', () => {
+  let title = 'Clicked!';
+  let options = {
+    body: 'This came from the button.'
+  }
+  showNotification(title, options);
+});
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('./service-worker.js')
       .then(registration => console.log('Service worker registered.'))
-      .catch(error => console.error(err));
+      .catch(error => console.error(error));
   });
 }
